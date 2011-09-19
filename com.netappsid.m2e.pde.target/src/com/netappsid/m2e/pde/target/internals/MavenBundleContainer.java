c149fae0edebaddd9b443fa65c16819f808670e3
Xavier Jodoin <xjodoin@gmail.com>
2011-09-19 15:25:13 -0400

    scope the action view to maven nature

--- com.netappsid.m2e.pde.target/src/com/netappsid/m2e/pde/target/internals/MavenBundleContainer.java
+++ com.netappsid.m2e.pde.target/src/com/netappsid/m2e/pde/target/internals/MavenBundleContainer.java
@@ -5,16 +5,32 @@ import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
 
+import org.apache.maven.RepositoryUtils;
 import org.apache.maven.artifact.Artifact;
-import org.apache.maven.project.MavenProject;
+import org.apache.maven.artifact.ArtifactUtils;
+import org.apache.maven.project.ProjectBuildingException;
+import org.apache.maven.repository.internal.MavenRepositorySystemSession;
+import org.codehaus.plexus.PlexusContainer;
+import org.codehaus.plexus.component.repository.exception.ComponentLookupException;
 import org.eclipse.core.runtime.CoreException;
 import org.eclipse.core.runtime.IProgressMonitor;
 import org.eclipse.m2e.core.embedder.IMaven;
+import org.eclipse.m2e.core.internal.MavenPluginActivator;
 import org.eclipse.m2e.core.project.IMavenProjectFacade;
 import org.eclipse.pde.internal.core.ifeature.IFeatureModel;
 import org.eclipse.pde.internal.core.target.AbstractBundleContainer;
 import org.eclipse.pde.internal.core.target.provisional.IResolvedBundle;
 import org.eclipse.pde.internal.core.target.provisional.ITargetDefinition;
+import org.sonatype.aether.RepositorySystem;
+import org.sonatype.aether.RepositorySystemSession;
+import org.sonatype.aether.collection.CollectRequest;
+import org.sonatype.aether.collection.CollectResult;
+import org.sonatype.aether.collection.DependencyCollectionException;
+import org.sonatype.aether.graph.Dependency;
+import org.sonatype.aether.repository.LocalRepository;
+import org.sonatype.aether.repository.RemoteRepository;
+import org.sonatype.aether.spi.connector.ArtifactUpload;
+import org.sonatype.aether.util.artifact.DefaultArtifact;
 
 public class MavenBundleContainer extends AbstractBundleContainer {
 
@@ -38,16 +54,19 @@ public class MavenBundleContainer extends AbstractBundleContainer {
 
 		for (int i = 0; i < mavenProjects.length; i++) {
 			IMavenProjectFacade mavenProjectFacade = mavenProjects[i];
-			artifacts.addAll(mavenProjectFacade.getMavenProject().getArtifacts());
+			artifacts.addAll(mavenProjectFacade.getMavenProject(monitor).getArtifacts());
 		}
 
 		List<IResolvedBundle> resolvedBundles = new ArrayList<IResolvedBundle>();
 
 		for (Artifact artifact : artifacts) {
-			IResolvedBundle generateBundle = generateBundle(artifact.getFile());
+			if (artifact.getFile().getName().endsWith(".jar")) {
+				IResolvedBundle generateBundle = generateBundle(artifact.getFile());
 
-			if (generateBundle != null) {
-				resolvedBundles.add(generateBundle);
+				if (generateBundle != null) {
+					resolvedBundles.add(generateBundle);
+				}
+
 			}
 		}
 
